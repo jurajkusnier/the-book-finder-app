efc3af31a9e08acee5c65f74db25508de8b5b2eb
jurajkusnier <jurajkusnier@gmail.com>
2018-07-13 00:34:42 +0200

    Glide implementation

--- app/src/main/java/com/jurajkusnier/thebookfinderapp/ui/main/BooksListAdapter.kt
+++ app/src/main/java/com/jurajkusnier/thebookfinderapp/ui/main/BooksListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.jurajkusnier.thebookfinderapp.R
 import com.jurajkusnier.thebookfinderapp.data.model.Item
+import com.jurajkusnier.thebookfinderapp.utils.GlideApp
 import kotlinx.android.synthetic.main.book_item.view.*
 
 class BooksListAdapter(val books:List<Item>): RecyclerView.Adapter<BooksListAdapter.ViewHolder>() {
@@ -17,9 +18,10 @@ class BooksListAdapter(val books:List<Item>): RecyclerView.Adapter<BooksListAdap
 
     override fun getItemCount(): Int = books.size
 
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.view.textBookName.text = books[position].volumeInfo.title
+
+        GlideApp.with(holder.view).load(books[position].volumeInfo.imageLinks.thumbnail).into(holder.view.imageBookCover)
     }
 
     class ViewHolder(val view: View): RecyclerView.ViewHolder(view)
