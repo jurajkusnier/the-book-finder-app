eff126c4172259826362954b109cb464ead5f796
jurajkusnier <jurajkusnier@gmail.com>
2018-07-13 01:24:20 +0200

    Bugfix: Crash because of network error

--- app/src/main/java/com/jurajkusnier/thebookfinderapp/ui/main/MainViewModel.kt
+++ app/src/main/java/com/jurajkusnier/thebookfinderapp/ui/main/MainViewModel.kt
@@ -3,24 +3,39 @@ package com.jurajkusnier.thebookfinderapp.ui.main
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
 import com.jurajkusnier.thebookfinderapp.data.model.GoogleBooksResult
 import com.jurajkusnier.thebookfinderapp.data.repository.BookRepository
+import io.reactivex.disposables.Disposable
 
 class MainViewModel(private val bookRepository: BookRepository) : ViewModel( ) {
+
+    val TAG = MainViewModel::class.java.simpleName
 
     private val _results: MutableLiveData<GoogleBooksResult> = MutableLiveData()
     val results: LiveData<GoogleBooksResult>
         get() = _results
 
     var searchName = ""
+    var disposable:Disposable? = null
+
+    override fun onCleared() {
+        disposable?.dispose()
+        super.onCleared()
+    }
 
     fun findBooks(name:String?) {
-        if (name == null || searchName  == name) return
+        if (name == null || searchName == name) return
 
         searchName = name
 
-        val disposable = bookRepository.findBooks(name)?.subscribe {
-            _results.value = it
+        disposable?.dispose()
+
+        disposable = bookRepository.findBooks(name)?.subscribe({ data ->
+            _results.value = data
+        }, { error ->
+            Log.e(TAG, Log.getStackTraceString(error))
         }
+        )
     }
 }
